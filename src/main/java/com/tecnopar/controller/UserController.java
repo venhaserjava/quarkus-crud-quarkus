a086e9b1dc7b8b0dbf2b0ac1049849a31bcedff8
venhaserjava <venhaserjava@gmail.com>
2024-10-31 07:16:27 -0400

    UserController(GET) findAll,

--- src/main/java/com/tecnopar/controller/UserController.java
+++ src/main/java/com/tecnopar/controller/UserController.java
@@ -7,17 +7,28 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     //@Inject
     private  final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+    @GET
+    public Response findAll(
+            @QueryParam("page") @DefaultValue("0") Integer page,
+            @QueryParam("pageSize") @DefaultValue("10") Integer pageSize)
+    {
+        var users = userService.findAll(page,pageSize);
+        return Response.ok(users).build();
     }
 
     @POST
@@ -25,4 +36,5 @@ public class UserController {
    public Response create(UserEntity userEntity){
        return Response.ok(userService.create(userEntity)).build();
    }
+
 }
--- src/main/java/com/tecnopar/service/UserService.java
+++ src/main/java/com/tecnopar/service/UserService.java
@@ -2,11 +2,22 @@ package com.tecnopar.service;
 
 import com.tecnopar.entity.UserEntity;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 @ApplicationScoped
 public class UserService {
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
     public UserEntity create(UserEntity userEntity) {
         UserEntity.persist(userEntity);
         return userEntity;
+    }
+    public List<UserEntity> findAll(Integer page, Integer pageSize){
+        return UserEntity.findAll()
+                .page(page,pageSize)
+                .list();
     }
 }
